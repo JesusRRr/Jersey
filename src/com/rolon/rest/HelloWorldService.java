4ecac6e567abe9cd74ca30927aa34490407ec6b6
JesusRRr <rolonjesus1996@gmail.com>
2020-06-25 13:44:01 -0500

    chore: add gson dependency to project

--- src/com/rolon/rest/HelloWorldService.java
+++ src/com/rolon/rest/HelloWorldService.java
@@ -1,10 +1,15 @@
 package com.rolon.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+import com.rolon.rest.model.User;
 
 @Path("/")
 public class HelloWorldService {
@@ -13,6 +18,18 @@ public class HelloWorldService {
     @Path("/{name}")
     public Response sayHello(@PathParam("name") String msg) {
         String output = "Hello, " + msg + "!";
+        return Response.status(200).entity(output).build();
+    }
+    
+    @POST
+    @Path("/newuser")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response createUser(User user) {
+    	
+    	System.out.println(user);
+    	Gson gson = new Gson();
+    	String output = gson.toJson(user);
         return Response.status(200).entity(output).build();
     }
     
--- src/com/rolon/rest/app/HelloWorldApplication.java
+++ src/com/rolon/rest/app/HelloWorldApplication.java
@@ -1,11 +1,13 @@
 package com.rolon.rest.app;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class HelloWorldApplication extends ResourceConfig {
 	    public HelloWorldApplication() {
 	        // Define the package which contains the service classes.
 	        packages("com.rolon.rest");
+	        register(JacksonFeature.class);
 	    }
 	    
 }
