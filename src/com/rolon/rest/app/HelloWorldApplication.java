1f1f41793d1a95dacc423f9069c6fa66e83e9d47
JesusRRr <rolonjesus1996@gmail.com>
2020-06-24 19:07:23 -0500

    feature: HelloWorldService class created with its configuration class HelloWordlApplication

---
+++ src/com/rolon/rest/app/HelloWorldApplication.java
@@ -0,0 +1,11 @@
+package com.rolon.rest.app;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class HelloWorldApplication extends ResourceConfig {
+	    public HelloWorldApplication() {
+	        // Define the package which contains the service classes.
+	        packages("com.rolon.rest");
+	    }
+}
+
